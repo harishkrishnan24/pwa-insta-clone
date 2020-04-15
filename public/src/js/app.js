50aa1a2f146b37ecbae8f929e36b820e9bde2130
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-15 11:33:14 +0530

    Added catch block to handle error

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,19 @@
+let deferredPrompt;
+
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("/sw.js").then(() => {
-		console.log("Service Worker registered!");
-	});
+	navigator.serviceWorker
+		.register("/sw.js")
+		.then(() => {
+			console.log("Service Worker registered!");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
+
+window.addEventListener("beforeinstallprompt", (event) => {
+	console.log("Before install prompt fired");
+	event.preventDefault();
+	deferredPrompt = event;
+	return false;
+});
